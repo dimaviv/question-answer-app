7311a62266dceb4c619bcc597e56dafbf663827b
dimaviv <dimaviv1812@gmail.com>
2022-06-08 19:22:18 +0300

    add db config

--- db.js
+++ db.js
@@ -1,12 +1,25 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect:'postgres',
-        host:process.env.DB_HOST,
-        port:process.env.DB_PORT
-    }
-)
+
+let sequelize;
+
+if (process.env.NODE_ENV === "production"){
+    sequelize = new Sequelize(process.env.DATABASE_URL)
+}else{
+    sequelize = new Sequelize(
+        process.env.PG_DATABASE,
+        process.env.PG_USER,
+        process.env.PG_PASSWORD,
+        {
+            dialect:'postgres',
+            host:process.env.PG_HOST,
+            port:process.env.PG_PORT
+        }
+    )
+}
+
+
+
+module.exports = sequelize
+
+
