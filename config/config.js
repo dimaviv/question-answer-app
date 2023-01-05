e3d3a3422afd90583a8c048d9f6e4656979b1ffd
dimaviv <dimaviv1812@gmail.com>
2023-01-05 18:33:05 +0200

    config sequelize-cli

---
+++ config/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: "127.0.0.1",
+    port: "5433",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
