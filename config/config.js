ac12f16a85be58e1008c25de0cda630cd5120ca3
dimaviv <dimaviv1812@gmail.com>
2023-01-30 14:34:53 +0200

    add sequelize prod config

--- config/config.js
+++ config/config.js
@@ -1,31 +1,31 @@
 require('dotenv').config()
 module.exports = {
-  development: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: "127.0.0.1",
-    port: "5433",
-    dialect: "postgres",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+    development: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: "127.0.0.1",
+        port: "5433",
+        dialect: "postgres",
     },
-  },
+    test: {
+        username: "root",
+        password: null,
+        database: "database_test",
+        host: "127.0.0.1",
+        dialect: "mysql",
+    },
+    production: {
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        host: process.env.PGHOST,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    },
 };
