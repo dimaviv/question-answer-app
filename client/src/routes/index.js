b886f77ec6231e414a9a2174528b9af6264f8491
Smajery <vasiliew.sasha@gmail.com>
2023-02-24 12:37:54 +0100

    added structure

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,8 +1,11 @@
-import {ROUTE_HOME, ROUTE_LOGIN} from "../utils/consts";
+import {ROUTE_HOME, ROUTE_LOGIN, ROUTE_QUESTIONS} from "../utils/consts";
 import loadable from "@loadable/component";
 import LoaderPage from "../components/UI/LoaderPage/LoaderPage";
 
 const HomePage = loadable(() => import("../pages/HomePage/HomePage"), {
+    fallback: <LoaderPage/>
+});
+const QuestionsPage = loadable(() => import("../pages/QuestionsPage/QuestionsPage"), {
     fallback: <LoaderPage/>
 });
 const LoginPage = loadable(() => import("../pages/LoginPage/LoginPage"), {
@@ -16,6 +19,7 @@ const ErrorPage = loadable(() => import("../pages/ErrorPage/ErrorPage"), {
 export const publicRoutes = [
     {path: '/', component: HomePage},
     {path: ROUTE_HOME, component: HomePage},
+    {path: ROUTE_QUESTIONS, component: QuestionsPage},
     {path: ROUTE_LOGIN, component: LoginPage},
     {path: '*', component: ErrorPage},
 ]
@@ -23,5 +27,6 @@ export const publicRoutes = [
 export const privateRoutes = [
     {path: '/', component: HomePage},
     {path: ROUTE_HOME, component: HomePage},
+    {path: ROUTE_QUESTIONS, component: QuestionsPage},
     {path: '*', component: ErrorPage},
 ]
--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,3 +1,4 @@
 export const ROUTE_HOME = '/home'
+export const ROUTE_QUESTIONS = '/questions'
 export const ROUTE_LOGIN = '/login'
 
