41a58b1f1ad966f3ecca8bbd4e7c5860ebf54eca
Smajery <vasiliew.sasha@gmail.com>
2023-02-24 11:47:12 +0100

    fixed object sizes

--- client/src/store/reducers/categories/CategoriesSlice.js
+++ client/src/store/reducers/categories/CategoriesSlice.js
@@ -16,6 +16,12 @@ const initialState = {
         {id: 7, name: 'Ukrainian', img: AlgebraImg},
         {id: 8, name: 'Spanish', img: AlgebraImg},
         {id: 9, name: 'Geography', img: AlgebraImg},
+        {id: 10, name: 'Algebra', img: AlgebraImg},
+        {id: 11, name: 'Geometry', img: GeometryImg},
+        {id: 12, name: 'Biology', img: BiologyImg},
+        {id: 13, name: 'Chemistry', img: ChemistryImg},
+        {id: 14, name: 'Computer science', img: ComputerScienceImg},
+        {id: 15, name: 'English', img: AlgebraImg},
     ],
 }
 
