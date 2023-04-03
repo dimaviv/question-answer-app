d1fb71afa161de579d4dded1e1e0e8474fba72fe
dimaviv <dimaviv1812@gmail.com>
2023-04-03 13:33:26 +0300

    attr text options

--- migrations/20230106122924-create-question.js
+++ migrations/20230106122924-create-question.js
@@ -2,17 +2,17 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+    async up(queryInterface, Sequelize) {
 
-    await queryInterface.createTable('questions', {
-      id: {type: Sequelize.INTEGER, primaryKey:true, autoIncrement:true},
-      text: {type: Sequelize.STRING, unique:true},
-      isAnswered: {type: Sequelize.BOOLEAN, defaultValue:false},
-    });
+        await queryInterface.createTable('questions', {
+            id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+            text: {type: Sequelize.TEXT, allowNull: false},
+            isAnswered: {type: Sequelize.BOOLEAN, defaultValue: false},
+        });
 
-  },
+    },
 
-  async down (queryInterface) {
-    await queryInterface.dropTable('questions');
-  }
+    async down(queryInterface) {
+        await queryInterface.dropTable('questions');
+    }
 };
--- migrations/20230106122933-create-answer.js
+++ migrations/20230106122933-create-answer.js
@@ -2,18 +2,18 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+    async up(queryInterface, Sequelize) {
 
-    await queryInterface.createTable('answers', {
-      id: {type: Sequelize.INTEGER, primaryKey:true, autoIncrement:true},
-      text: {type: Sequelize.TEXT},
-      rating: {type: Sequelize.INTEGER, defaultValue:0},
-      isBest: {type: Sequelize.BOOLEAN, defaultValue:false}
-    });
+        await queryInterface.createTable('answers', {
+            id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+            text: {type: Sequelize.TEXT, allowNull: false},
+            rating: {type: Sequelize.INTEGER, defaultValue: 0},
+            isBest: {type: Sequelize.BOOLEAN, defaultValue: false}
+        });
 
-  },
+    },
 
-  async down (queryInterface) {
-    await queryInterface.dropTable('answers');
-  }
+    async down(queryInterface) {
+        await queryInterface.dropTable('answers');
+    }
 };
