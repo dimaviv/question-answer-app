d1d1489e31099fad8c936c40e3b2986db61e3e26
Smajery <vasiliew.sasha@gmail.com>
2023-04-14 15:49:29 +0200

    fixed code

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,5 +1,5 @@
 export const ROUTE_HOME = '/home';
 export const ROUTE_LOGIN = '/login';
 export const ROUTE_SIGNUP = '/signup';
-export const ROUTE_GOOGLE_AUTH = '/google_auth';
+export const ROUTE_GOOGLE_AUTH = '/google-auth';
 export const ROUTE_ERROR = '/error';
